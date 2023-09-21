5098446eb7b7c5aeeeb4e84c4591f33326cbb549
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-09-21 15:04:50 +0100

    snagging

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # from django.http import HttpResponse
 from.models import Listing
@@ -6,9 +6,8 @@ from.models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
-
-    paginator = Paginator(listings, 3)
+    listings = Listing.objects.all().order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
@@ -18,7 +17,11 @@ def index(request):
     return render(request, 'listings/index.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
